2cb9ab9861aa72f7ca55a60eda13729811c503c7
emPeeGee <eempeegee@gmail.com>
2024-10-11 17:11:19 +0300

    chore:  🐎 Clean up default code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div
+      class="flex w-full max-w-xl h-screen items-center justify-center mx-auto bg-green-400 border-b border-gray-600"
+    >
+      Post
+    </div>
   </main>
 </template>
