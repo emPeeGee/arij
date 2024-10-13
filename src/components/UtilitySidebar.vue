b2c68cad23809e6c731ada0c953a43262bc5ee70
emPeeGee <eempeegee@gmail.com>
2024-10-13 22:11:33 +0300

    feat: create the utility sidebar

--- src/components/UtilitySidebar.vue
+++ src/components/UtilitySidebar.vue
@@ -1,12 +1,96 @@
 <script setup lang="ts"></script>
 <template>
   <aside class="flex flex-row h-full">
-    <nav class="flex w-16 h-full bg-blue-500">
-      <div class="w-full flex mx-auto px-6 py-8">
-        <div
-          class="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed"
-        >
-          Sidebar
+    <nav class="flex w-16 h-full bg-primary-900">
+      <div class="w-full flex mx-auto py-8">
+        <div class="w-full h-full flex flex-col items-center text-gray-900 text-xl">
+          <div class="w-full mb-auto">
+            <div class="flex items-center justify-center">
+              <svg
+                class="w-7 h-7 text-gray-800 dark:text-primary-50 mb-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 18.5A2.493 2.493 0 0 1 7.51 20H7.5a2.468 2.468 0 0 1-2.4-3.154 2.98 2.98 0 0 1-.85-5.274 2.468 2.468 0 0 1 .92-3.182 2.477 2.477 0 0 1 1.876-3.344 2.5 2.5 0 0 1 3.41-1.856A2.5 2.5 0 0 1 12 5.5m0 13v-13m0 13a2.493 2.493 0 0 0 4.49 1.5h.01a2.468 2.468 0 0 0 2.403-3.154 2.98 2.98 0 0 0 .847-5.274 2.468 2.468 0 0 0-.921-3.182 2.477 2.477 0 0 0-1.875-3.344A2.5 2.5 0 0 0 14.5 3 2.5 2.5 0 0 0 12 5.5m-8 5a2.5 2.5 0 0 1 3.48-2.3m-.28 8.551a3 3 0 0 1-2.953-5.185M20 10.5a2.5 2.5 0 0 0-3.481-2.3m.28 8.551a3 3 0 0 0 2.954-5.185"
+                />
+              </svg>
+            </div>
+
+            <div class="w-full flex flex-col">
+              <div
+                class="w-full flex items-center justify-center py-2 hover:bg-white/10 cursor-pointer"
+              >
+                <svg
+                  class="w-6 h-6 text-gray-800 dark:text-primary-50"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-width="2"
+                    d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <div
+                class="w-full flex items-center justify-center py-2 hover:bg-white/10 cursor-pointer"
+              >
+                <svg
+                  class="w-6 h-6 text-gray-800 dark:text-primary-50"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 12h14m-7 7V5"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="w-full flex items-center justify-center py-2 hover:bg-white/10 cursor-pointer"
+          >
+            <svg
+              class="w-6 h-6 text-gray-800 dark:text-primary-50"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
+const colors = require('tailwindcss/colors')
+
+const customColors = {
+  blue: {
+    50: '#e3f2fe',
+    100: '#bbdefd',
+    200: '#8fcafc',
+    300: '#61b5fa',
+    400: '#3ca5fa',
+    500: '#1396f8',
+    600: '#1388ea',
+    700: '#1076d7',
+    800: '#0d65c5',
+    900: '#0747a6'
+  }
+}
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      ...colors,
+      ...customColors,
+      primary: customColors.blue
+    },
     extend: {}
   },
   plugins: []
