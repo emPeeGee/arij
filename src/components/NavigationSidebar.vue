ae337cff369a2fb1669072f3fe5703ba07e82967
emPeeGee <eempeegee@gmail.com>
2024-10-14 20:27:52 +0300

    ✨ feat: basic layout for navigation sidebar

--- src/components/NavigationSidebar.vue
+++ src/components/NavigationSidebar.vue
@@ -1,11 +1,56 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CogIcon from '@/components/icons/CogIcon.vue'
+import ColumnIcon from '@/components/icons/ColumnIcon.vue'
+</script>
+
 <template>
-  <nav class="flex w-72 h-full bg-gray-300">
-    <div class="w-full flex mx-auto px-6 py-8">
-      <div
-        class="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed"
-      >
-        Sidebar
+  <nav class="flex w-72 h-full bg-slate-200">
+    <div class="w-full flex mx-auto px-4 py-8">
+      <div class="w-full h-full flex flex-col text-gray-900 text-xl">
+        <div class="flex mb-6 gap-2">
+          <div class="flex items-center">
+            <svg
+              class="w-10 h-10 text-green-800"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M14.5 3 12 7.156 9.857 3H2l10 18L22 3h-7.5ZM4.486 4.5h2.4L12 13.8l5.107-9.3h2.4L12 18.021 4.486 4.5Z"
+              />
+            </svg>
+          </div>
+          <div class="flex flex-col">
+            <p class="text-base font-semibold text-slate-700">Vue Jira Clone</p>
+            <p class="text-sm text-slate-600">Software Project</p>
+          </div>
+        </div>
+
+        <div class="flex flex-col">
+          <div class="flex justify-start items-center gap-2 p-2">
+            <ColumnIcon />
+            <p class="text-base text-gray-700">Kanban board</p>
+          </div>
+
+          <div class="flex justify-start items-center gap-2 p-2 rounded bg-slate-300">
+            <CogIcon highlight />
+            <p class="text-base text-blue-800">Project settings</p>
+          </div>
+
+          <hr class="my-4 h-0.5 border-t-0 bg-slate-300 dark:bg-slate-400" />
+
+          <div
+            v-for="item in [1, 2, 3, 4, 5]"
+            :key="item"
+            class="flex justify-start items-center gap-2 p-2"
+          >
+            <ColumnIcon />
+            <p class="text-base text-gray-700">Kanban board</p>
+          </div>
+        </div>
       </div>
     </div>
   </nav>
