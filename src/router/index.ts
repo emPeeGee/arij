0cee6856525c23b523330735bfcce0e748472e9b
emPeeGee <eempeegee@gmail.com>
2024-10-17 22:33:56 +0300

    feat✨: restructure app routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../components/NotFound.vue'
 
+// TODO: revise routing one more time
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: 'kanban' },
     { path: '/home', name: 'home', component: HomeView },
+    {
+      path: '/projects',
+      name: 'Projects',
+      meta: { breadCrumb: 'Projects' },
+      // component: null,
+      children: [
+        {
+          path: ':id',
+          name: 'Id',
+          // component: null,
+          children: [
+            {
+              path: 'kanban',
+              name: 'Kanban board',
+              meta: { breadCrumb: 'Kanban board' },
+              component: () => import('../views/KanbanBoardView.vue')
+            },
+            {
+              path: 'settings',
+              name: 'Settings',
+              meta: { breadCrumb: 'Settings' },
+              component: () => import('../views/ProjectSettingsView.vue')
+            },
+            {
+              path: '/:pathMatch(.*)*',
+              component: NotFound
+            }
+          ]
+        }
+      ]
+    },
     {
       path: '/about',
       name: 'about',
@@ -13,16 +46,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/kanban',
-      name: 'kanbanBoard',
-      component: () => import('../views/KanbanBoardView.vue')
-    },
-    {
-      path: '/projectSettings',
-      name: 'projectSettings',
-      component: () => import('../views/ProjectSettingsView.vue')
     }
   ]
 })
