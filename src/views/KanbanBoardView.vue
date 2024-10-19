f56aa2a0353614fb8fd714f3e624bc15f4a8e3dc
emPeeGee <eempeegee@gmail.com>
2024-10-19 14:55:20 +0300

    🐞 fix: layout with inputs in the kanban board

--- src/views/KanbanBoardView.vue
+++ src/views/KanbanBoardView.vue
@@ -175,38 +175,40 @@ const onDragOver = (event: DragEvent, cardIndex: number, colIndex: number) => {
         </Button>
       </div>
     </div>
-    <form class="max-w-44">
-      <label
-        for="default-search"
-        class="mb-2 text-sm font-medium text-slate-900 sr-only dark:text-white"
-        >Search</label
-      >
-      <div class="relative">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <svg
-            class="w-4 h-4 text-slate-500 dark:text-slate-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
+    <div class="flex justify-start gap-4">
+      <form class="max-w-44">
+        <label
+          for="default-search"
+          class="mb-2 text-sm font-medium text-slate-900 sr-only dark:text-white"
+          >Search</label
+        >
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              class="w-4 h-4 text-slate-500 dark:text-slate-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            class="block w-full py-1.5 px-4 ps-9 text-sm text-slate-900 border border-slate-300 rounded-lg bg-slate-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-slate-700 dark:border-slate-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
         </div>
-        <input
-          type="search"
-          class="block w-full py-1.5 px-4 ps-9 text-sm text-slate-900 border border-slate-300 rounded-lg bg-slate-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-slate-700 dark:border-slate-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        />
-      </div>
-
+      </form>
       <MembersSelect />
-    </form>
+      <Button label="Only my issues" />
+    </div>
 
     <div class="flex items-center justify-center">
       <div class="h-full overflow-auto flex flex-row flex-nowrap gap-2">
